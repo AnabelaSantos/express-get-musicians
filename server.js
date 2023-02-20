bce297ced5724e9ce65e3109eb7aa74f9752f903
anabelaSantos <anabelamsantos@gmail.com>
2023-02-20 17:11:56 +0000

    exercise complete

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
-const {Musician} = require("./Musician")
-const {sequelize} = require("./db")
+const { Musician } = require("./Musician");
+const { sequelize } = require("./db");
+const { response } = require("express");
 
 const port = 3000;
 
 //TODO
+app.get("/musicians", async (request, response) => {
+  const musician = await Musician.findAll();
+  response.json(musician);
+});
 
 app.listen(port, () => {
-    sequelize.sync();
-    console.log(`Listening on port ${port}`)
-})
+  sequelize.sync();
+  console.log(`Listening on port ${port}`);
+});
