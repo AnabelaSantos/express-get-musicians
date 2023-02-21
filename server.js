becb978a090aba68bcb1b525d10849723102630f
anabelaSantos <anabelamsantos@gmail.com>
2023-02-21 14:57:47 +0000

    exercise day 2 complete

--- server.js
+++ server.js
@@ -7,10 +7,24 @@ const { response } = require("express");
 const port = 3000;
 
 //TODO
-app.get("/musicians", async (request, response) => {
-  const musician = await Musician.findAll();
+app.get("/musicians/:id", async (request, response, next) => {
+  const musician = await Musician.findByPk(request.params.id); // retrieve musician object from the DB by its id
   response.json(musician);
 });
+
+// app.get("/musicians/:id", async (request, response, next) => {
+//   try {
+//     const musician = await Musician.findByPk(request.params.id);
+//     if (!musician) {
+//       response.status(404).send("MUsician not found"); // there's no musician in the requested id so a 404 error is responded and a message sent
+//     } else {
+//       response.json(musician); // sends the respective data of that id
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     response.status(500).send("Internal server error");
+//   }
+// });
 
 app.listen(port, () => {
   sequelize.sync();
