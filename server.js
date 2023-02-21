2877d8f02ce36a3869d1301dd540e23752473332
anabelaSantos <anabelamsantos@gmail.com>
2023-02-21 17:06:36 +0000

    error messages updated

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ app.get("/musicians/:id", async (request, response, next) => {
     const musician = await Musician.findByPk(request.params.id);
     if (!musician) {
       throw new Error("Musician id not found");
-      // response.status(404).send("Musician not found"); // there's no musician in the requested id so a 404 error is responded and a message sent
+      // there's no musician in the requested id so a 404 error is responded and a message sent
     }
     response.json(musician); // sends the respective data of that id
-  } catch (err) {
-    console.log(err);
-    response.status(500).send("Internal server error");
+  } catch (error) {
+    // console.log(err);
+    response.status(404).send(error.message);
   }
 });
 
