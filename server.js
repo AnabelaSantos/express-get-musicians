772479953723bf88f541fd19125d1efb78a18424
anabelaSantos <anabelamsantos@gmail.com>
2023-02-21 16:50:04 +0000

    error message

--- server.js
+++ server.js
@@ -7,24 +7,24 @@ const { response } = require("express");
 const port = 3000;
 
 //TODO
-app.get("/musicians/:id", async (request, response, next) => {
-  const musician = await Musician.findByPk(request.params.id); // retrieve musician object from the DB by its id
-  response.json(musician);
-});
-
 // app.get("/musicians/:id", async (request, response, next) => {
-//   try {
-//     const musician = await Musician.findByPk(request.params.id);
-//     if (!musician) {
-//       response.status(404).send("MUsician not found"); // there's no musician in the requested id so a 404 error is responded and a message sent
-//     } else {
-//       response.json(musician); // sends the respective data of that id
-//     }
-//   } catch (err) {
-//     console.log(err);
-//     response.status(500).send("Internal server error");
-//   }
+//   const musician = await Musician.findByPk(request.params.id); // retrieve musician object from the DB by its id
+//   response.json(musician);
 // });
+
+app.get("/musicians/:id", async (request, response, next) => {
+  try {
+    const musician = await Musician.findByPk(request.params.id);
+    if (!musician) {
+      throw new Error("Musician id not found");
+      // response.status(404).send("Musician not found"); // there's no musician in the requested id so a 404 error is responded and a message sent
+    }
+    response.json(musician); // sends the respective data of that id
+  } catch (err) {
+    console.log(err);
+    response.status(500).send("Internal server error");
+  }
+});
 
 app.listen(port, () => {
   sequelize.sync();
